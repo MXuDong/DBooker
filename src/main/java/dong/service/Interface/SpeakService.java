35cc3eb5fe3113026a211178efece464ef7b6f75
MXuDong <2941884109@qq.com>
2018-07-24 23:10:24 +0800

    SpeakBooker Create

--- src/main/java/dong/service/Interface/SpeakService.java
+++ src/main/java/dong/service/Interface/SpeakService.java
@@ -1,4 +1,19 @@
 package dong.service.Interface;
 
+import dong.model.SpeakBooker;
+import dong.model.User;
+
+import java.util.List;
+
 public interface SpeakService {
+//    添加评论
+    boolean createSpeak(SpeakBooker speakBooker);
+//    取消评论
+    boolean deleteSpeak(SpeakBooker speakBooker);
+//    修改评论
+    boolean updateSpeakBooker(SpeakBooker speakBooker);
+//    根据Booker查找所有评论
+    List<SpeakBooker> findAllByBookerId(int bookerId);
+//    查找评论的用户ＩＤ
+    User findUserBySpeak(SpeakBooker speakBooker);
 }
