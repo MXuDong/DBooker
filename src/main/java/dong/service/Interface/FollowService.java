4bad2f3f2f0a0410e3d8af9b034ff83dc6bb77c9
MXuDong <2941884109@qq.com>
2018-07-24 23:07:59 +0800

    Create FollowService

--- src/main/java/dong/service/Interface/FollowService.java
+++ src/main/java/dong/service/Interface/FollowService.java
@@ -1,4 +1,17 @@
 package dong.service.Interface;
 
+import dong.model.Follow;
+import dong.model.User;
+
 public interface FollowService {
+//    添加关注
+    boolean createFollow(Follow follow);
+//    取消关注
+    boolean deleteFollow(Follow follow);
+//    查看关注者
+    User findFollowUser(Follow follow);
+//    查看被关注者
+    User findFollowedUser(Follow follow);
+
 }
+
