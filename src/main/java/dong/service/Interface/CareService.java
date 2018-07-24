74092467187c74e0008ff2604ae2a8ad24ec6fc9
MXuDong <2941884109@qq.com>
2018-07-24 23:00:39 +0800

    CareService Create

--- src/main/java/dong/service/Interface/CareService.java
+++ src/main/java/dong/service/Interface/CareService.java
@@ -1,4 +1,16 @@
 package dong.service.Interface;
 
+import dong.model.Bookers;
+import dong.model.Care;
+import dong.model.User;
+
 public interface CareService {
+//    添加收藏
+    boolean createCare(Care care);
+//    取消收藏
+    boolean deleteCare(Care care);
+//    查看关注人
+    User getCareUser(Care care);
+//    查看被关注博客
+    Bookers getBeCaredBooker(Care care);
 }
