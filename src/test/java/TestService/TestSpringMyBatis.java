a488a49ce58cc11323603396b9e2be62947418e8
MXuDong <2941884109@qq.com>
2018-07-24 16:54:39 +0800

    测试UserServiceI完成

--- src/test/java/TestService/TestSpringMyBatis.java
+++ src/test/java/TestService/TestSpringMyBatis.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.List;
+
 public class TestSpringMyBatis {
 
     private ApplicationContext ac;
@@ -16,6 +18,18 @@ public class TestSpringMyBatis {
     public void prefer(){
         ac = new ClassPathXmlApplicationContext(new String[] {"applicationContext.xml"});
         serviceI = (UserServiceI) ac.getBean("userService");
+    }
+
+    @Test
+    public void TestGetUserByUserName(){
+        User u = serviceI.findUserByUserName("mxd");
+        System.out.println(u);
+    }
+
+    @Test
+    public void TestFindUserById(){
+        User u = serviceI.findUserById(1);
+        System.out.println(u);
     }
 
     @Test
