f544e78eb055734e2a8277b27f7c63fc0a6294b0
MXuDong <2941884109@qq.com>
2018-07-27 00:42:40 +0800

    SSM 初步整合测试完成

--- src/main/java/dong/controller/IndexController.java
+++ src/main/java/dong/controller/IndexController.java
@@ -1,4 +1,18 @@
 package dong.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("")
 public class IndexController {
+
+    @RequestMapping("index")
+    public ModelAndView listCategory(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
 }
