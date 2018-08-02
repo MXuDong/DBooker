0c2bd5647261af961b292872db7e6f94ea93301b
MXuDong <2941884109@qq.com>
2018-08-02 10:07:58 +0800

    Test BootStrap Complete

--- src/main/java/dong/controller/defaultController.java
+++ src/main/java/dong/controller/defaultController.java
@@ -1,4 +1,17 @@
 package dong.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("")
 public class defaultController {
+
+    @RequestMapping(value = {"", "index.jsp"})
+    public ModelAndView turnToIndexJsp(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
 }
